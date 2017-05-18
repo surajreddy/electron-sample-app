0f4f2a1007549be1073401e31f88a73375b0373e
Suraj Reddy <suraj.reddy@shopify.com>
2017-05-18 16:00:48 -0400

    Change showMessageBox to confirm

--- webview-preload.js
+++ webview-preload.js
@@ -1,22 +1,7 @@
-const {dialog} = require('electron').remote;
-
-function confirmNavigation() {
-  const choice = dialog.showMessageBox({
-    type: 'question',
-    buttons: ['Leave', 'Stay'],
-    title: 'Do you want to leave this site?',
-    message: 'Changes you made may not be saved.',
-    defaultId: 0,
-    cancelId: 1
-  });
-
-  return choice === 0;
-}
-
 window.addEventListener(
   'beforeunload',
   (event) => {
-    confirmNavigation();
-    event.returnValue = 'stop navigation'; // should stop navigation, but doesn't if called after calling showMessageBox.
+    const userChoice = confirm('Are you sure you want to navigate away?');
+    event.returnValue = 'stop navigation'; // Should stop navigation, but doesn't after calling confirm.
   }
 );
