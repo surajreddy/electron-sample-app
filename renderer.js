f43afdbdc2d21b45392f146496d546f880abc62b
Suraj Reddy <suraj.reddy@shopify.com>
2017-05-18 15:19:32 -0400

    Update code to reflect showMessageBox not blocking

--- renderer.js
+++ renderer.js
@@ -2,28 +2,5 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 const webview = document.querySelector('webview');
-const {dialog} = require('electron').remote;
 
 webview.src=`file://${__dirname}/webview-src.html`;
-
-webview.addEventListener('dom-ready', () => {
-  setupPreventUnloadHandler();
-});
-
-function setupPreventUnloadHandler() {
-  const contents = webview.getWebContents();
-
-  contents.on('will-prevent-unload', (event) => {
-    const choice = dialog.showMessageBox({
-      type: 'question',
-      buttons: ['Leave', 'Stay'],
-      title: 'Do you want to leave this site?',
-      message: 'Changes you made may not be saved.',
-      defaultId: 0,
-      cancelId: 1
-    });
-    if (choice === 0) {
-      event.preventDefault(); // Should allow navigation, but doesn't.
-    }
-  });
-}
--- webview-preload.js
+++ webview-preload.js
@@ -1,6 +1,25 @@
+const {dialog} = require('electron').remote;
+
+function confirmNavigation() {
+  const choice = dialog.showMessageBox({
+    type: 'question',
+    buttons: ['Leave', 'Stay'],
+    title: 'Do you want to leave this site?',
+    message: 'Changes you made may not be saved.',
+    defaultId: 0,
+    cancelId: 1
+  }); // Should block navigation, but doesn't.
+
+  return choice === 1;
+}
+
 window.addEventListener(
   'beforeunload',
   (event) => {
-    event.returnValue = 'stop navigation';
+    if (confirmNavigation()) {
+      // return nothing - allows navigation
+    } else {
+      event.returnValue = 'stop navigation';
+    }
   }
 );
